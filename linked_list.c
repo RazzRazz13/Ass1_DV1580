e408ee836224a39e2bb15cf15c22ad82bdccaa2a
RazzRazz13 <RazzRazz34@gmail.com>
2024-10-01 16:41:19 +0200

    Gonna test to submit

--- linked_list.c
+++ linked_list.c
@@ -8,37 +8,33 @@ typedef struct Node {
   uint16_t data; // Stores the data as an unsigned 16-bit integer
   struct Node* next; // A pointer to the next node in the List
 }Node;
-  
-
-static Node* firstnode;
 
 void list_init(Node** head, size_t size){
-  mem_init(size);
-  *head = NULL;
+  mem_init(size); //Initializes the memory manager
+  *head = NULL; //Sets head to null
 }
 
 void list_insert(Node** head, uint16_t data){
 
-  Node* next_node = (Node*)mem_alloc(sizeof(Node));
+  Node* next_node = (Node*)mem_alloc(sizeof(Node)); //Creating a new node by allocting memory in memory manger
   (*next_node).data = data;
   (*next_node).next = NULL;
   
   Node* current_node = *head;
 
-  if (!current_node){
-    *head = next_node;
+  if (!current_node){ //Checking if the new node is the first node
+    *head = next_node; //Setting the new node to the first node
     return;
   }
 
-  while((*current_node).next){
+  while((*current_node).next){ //Iterates the whole list
     current_node = (*current_node).next;
   }
 
-  (*current_node).next = next_node;
+  (*current_node).next = next_node; //Adding the new node last in the list
 }
 
 void list_insert_after(Node* prev_node, uint16_t data){
-
   Node* next_node = (Node*)mem_alloc(sizeof(Node));
 
   (*next_node).data = data;
@@ -48,54 +44,53 @@ void list_insert_after(Node* prev_node, uint16_t data){
 }
 
 void list_insert_before(Node** head, Node* next_node, uint16_t data){
-
-  Node* prev_node = (Node*)mem_alloc(sizeof(Node));
-
+  Node* prev_node = (Node*)mem_alloc(sizeof(Node)); //Creating a new node by allocting memory in memory manger
   (*prev_node).data = data;
   (*prev_node).next = next_node;
 
   Node* current_node = *head;
 
-  if (current_node == next_node){
-    *head = prev_node;
+  if (current_node == next_node){ //Checking if the first node is the node we are searching for
+    *head = prev_node; //Setting the new node to the first node
     return;
   }
 
-  while((*current_node).next != next_node){
+  while((*current_node).next != next_node){ //Iterates the list until we find the searched node
     current_node = (*current_node).next;
   }
-  (*current_node).next = prev_node;
-
-  
+  (*current_node).next = prev_node; //Setting the node before the searched node next value to the new node
+  return;
 }
 
 void list_delete(Node** head, uint16_t data){
   Node* current_node = *head;
   Node* previous_node;
 
-  if((*current_node).data == data){
-    *head = (*current_node).next;
-    mem_free((char*)current_node);
+  if((*current_node).data == data){ //Checking if first node is the searched node
+    *head = (*current_node).next; //Setting second node as first node
+    mem_free((char*)current_node); //Freeing the space from the first node
     return;
   }
 
-  while((*current_node).data != data){
-    previous_node = current_node;
+  while((*current_node).data != data){ //Iterates the list until we find the node with the searched data
+    previous_node = current_node;  
     current_node = (*current_node).next;
   }
 
-  (*previous_node).next = (*current_node).next;
-  mem_free((char*)current_node);
+  (*previous_node).next = (*current_node).next; //Removing the node from the list
+  mem_free((char*)current_node); //Freeing the memory of the node
 }
 
 Node* list_search(Node** head, uint16_t data){
   Node* current_node = *head;
-  while(current_node){
-    if((*current_node).data == data){
+
+  while(current_node){ //Iterates the list until we find the node with the searched data
+    if ((*current_node).data == data){
       return current_node;
     }
     current_node = (*current_node).next;
   }
+  return NULL;
 }
 
 void list_display(Node** head){
@@ -137,38 +132,36 @@ void list_display_range(Node** head, Node* start_node, Node* end_node){
 int list_count_nodes(Node** head){
   Node* current_node = *head;
   int count = 0;
-  while(current_node){
-      count = ++count;
+
+  while(current_node){ //Iterates all nodes in the list
+      count = count + 1; //Adds to the count
+      printf("%d\n", (*current_node).data);
       current_node = (*current_node).next;
+      return 3;
   }
   return count;
 }
 
 void list_cleanup(Node** head){
-  Node* current_node = *head;
-  while(current_node){
-    Node* next_node = (*current_node).next;
-    mem_free((char*)current_node);
-    current_node = next_node;
-  }
-  *head = NULL;
+  *head = NULL; //Setting the head of the list to NULL
   mem_deinit();
 }
 
 int main(){
+  printf(" Testing list_count_nodes ---> ");
   Node *head = NULL;
-  list_init(&head, sizeof(Node) *6);
+  list_init(&head, sizeof(Node) * 3);
   list_insert(&head, 10);
   list_insert(&head, 20);
   list_insert(&head, 30);
-  list_insert(&head, 40);
-  list_insert(&head, 50);
-  list_insert(&head, 60);
-  Node *yes = list_search(&head, 20);
-  Node *no = list_search(&head, 50);
-  list_display_range(&head,yes,no);
 
-
-
-
+  printf(" Testing list_count_nodes ---> ");
+  int count = list_count_nodes(&head);
+  printf(" Testing list_count_nodes ---> ");
+  assert(count == 3);
+  
+  
+  
+  list_cleanup(&head);
+  printf("[PASS].\n");
 }
--- memory_manager.c
+++ memory_manager.c
@@ -2,22 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <assert.h>
 
 typedef struct Memory_Block {
-    int free;
-    size_t size;
-    struct Memory_Block* next;
+    int free;  //Stores bool to show if its free
+    size_t size; //Stores the size of the memory block
+    struct Memory_Block* next; //Stores the next memory block
 } Memory_Block;
 
 static Memory_Block* first_block;
 static void* memorypool;
 
-
-
 void mem_init(size_t size){
-    memorypool = malloc(size);
-    first_block = (Memory_Block*)(char*)memorypool + sizeof(Memory_Block);
+    memorypool = malloc(size); //Initialize the memeorypool
+    first_block = (Memory_Block*)(char*)memorypool + sizeof(Memory_Block); //Create the first empty block
     (*first_block).free = 1;
     (*first_block).size = size;
     (*first_block).next = NULL;
@@ -26,25 +23,25 @@ void mem_init(size_t size){
 void* mem_alloc(size_t size) {
 
     if (size == 0) {
-        return NULL; 
+        return NULL;  //If trying to allocate 0 bytes reutn the memorypool
     }
     
     Memory_Block* current_block = first_block;
 
-    while(current_block){
+    while(current_block){ //Iterates all existing blocks in memorypool
         if ((*current_block).free == 1 && (*current_block).size >= size){
-            if ((*current_block).size > size){
-                Memory_Block* next_block = (Memory_Block*)((char*)current_block + size + sizeof(Memory_Block));
+            if ((*current_block).size > size){ //If the current block is free and its size is greater than the allocating size: use it 
+                Memory_Block* next_block = (Memory_Block*)((char*)current_block + size + sizeof(Memory_Block)); //Creating new empty block
                 (*next_block).free = 1;
                 (*next_block).size = (*current_block).size - size;
                 (*next_block).next = (*current_block).next;
                 (*current_block).free = 0;
                 (*current_block).size = size;
-                (*current_block).next = next_block;
+                (*current_block).next = next_block; //Setting the found block as allocated han connecting it to the new empty block
             } else {
-                (*current_block).free = 0;
+                (*current_block).free = 0; //This will be run when current block is equal in size to the allocating
             }
-            return (char*)current_block;
+            return (char*)current_block; //Returns pointer to the allocated block
         }
         current_block = (*current_block).next;
     }
@@ -52,36 +49,30 @@ void* mem_alloc(size_t size) {
 
 void mem_free(void* block) {
     Memory_Block* current_block = (Memory_Block*)((char*)block);
-    (*current_block).free = 1;
+    (*current_block).free = 1; //Sets the intended block as free
 }
 
 void* mem_resize(void* block, size_t size){
     Memory_Block* current_block = (Memory_Block*)block;
 
     if((*current_block).size < size){
-        Memory_Block* next_block = (Memory_Block*)((char*)current_block + size);
+        Memory_Block* next_block = (Memory_Block*)((char*)current_block + size); //Creating new empty block
         (*next_block).free = 1;
         (*next_block).size = (*current_block).size - size;
         (*next_block).next = (*current_block).next;
 
         (*current_block).free = 0;
         (*current_block).size = size;
-        (*current_block).next = next_block;
+        (*current_block).next = next_block; //Splitting one bock into two blocks with one as the intended size
 
     }else if ((*current_block).size > size){
-        Memory_Block* new_block = mem_alloc(size);
-        mem_free(current_block);
-        current_block = new_block;
+        Memory_Block* new_block = mem_alloc(size); //Getting a new block with the intended size
+        mem_free(current_block); // Freeing the old block
+        current_block = new_block; //Returning the new block
     }
     return current_block;
 }
 
 void mem_deinit(){
-    free(memorypool);
+    free(memorypool); //Freeing the memorypool
 }
-
-
-
-
-
-
