10a82b470a5c1aa46af3a45aa8afb60ed2640888
RazzRazz13 <RazzRazz34@gmail.com>
2024-10-14 12:19:24 +0200

    testing

--- memory_manager.c
+++ memory_manager.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <pthread.h>
 
 typedef struct Memory_Block {
     int free;  //Stores bool to show if its free
@@ -11,6 +12,7 @@ typedef struct Memory_Block {
 
 static Memory_Block* first_block;
 static void* memorypool;
+pthread_mutex_t mutex;
 
 void mem_init(size_t size){
     memorypool = malloc(size); //Initialize the memeorypool
@@ -22,12 +24,13 @@ void mem_init(size_t size){
 
 void* mem_alloc(size_t size) {
 
+    pthread_mutex_lock(&mutex);
     if (size == 0) {
+        pthread_mutex_unlock(&mutex);
         return NULL;  //If trying to allocate 0 bytes reutn the memorypool
     }
     
     Memory_Block* current_block = first_block;
-
     while(current_block){ //Iterates all existing blocks in memorypool
         if ((*current_block).free == 1 && (*current_block).size >= size){
             if ((*current_block).size > size){ //If the current block is free and its size is greater than the allocating size: use it 
@@ -41,14 +44,17 @@ void* mem_alloc(size_t size) {
             } else {
                 (*current_block).free = 0; //This will be run when current block is equal in size to the allocating
             }
+            pthread_mutex_unlock(&mutex);
             return (char*)current_block + sizeof(Memory_Block); //Returns pointer to the allocated block
         }
         current_block = (*current_block).next;
     }
-    //return;
+    pthread_mutex_unlock(&mutex);
+    return NULL;
 }
 
 void mem_free(void* block) {
+
     if(block == NULL){ //Returns if trying to free NULL
         return;
     }
@@ -93,6 +99,7 @@ void* mem_resize(void* block, size_t size){
     Memory_Block* current_block = (Memory_Block*)block;
 
     if((*current_block).size < size){
+        pthread_mutex_lock(&mutex);
         Memory_Block* next_block = (Memory_Block*)((char*)current_block + size); //Creating new empty block
         (*next_block).free = 1;
         (*next_block).size = (*current_block).size - size;
@@ -101,10 +108,11 @@ void* mem_resize(void* block, size_t size){
         (*current_block).free = 0;
         (*current_block).size = size;
         (*current_block).next = next_block; //Splitting one bock into two blocks with one as the intended size
+        pthread_mutex_unlock(&mutex);
 
     }else if ((*current_block).size > size){
-        Memory_Block* new_block = mem_alloc(size); //Getting a new block with the intended size
         mem_free(current_block); // Freeing the old block
+        Memory_Block* new_block = mem_alloc(size); //Getting a new block with the intended size
         current_block = new_block; //Returning the new block
     }
     return current_block;
